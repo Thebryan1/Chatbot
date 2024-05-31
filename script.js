797a9ab01353937237c5ade1c4ba24b04237d769
Thebryan1 <130424766+Thebryan1@users.noreply.github.com>
2024-05-31 00:57:58 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const responses = {
     greetings: ['Hola!', '¡Hola! ¿Cómo estás?', '¿Qué tal?', '¡Saludos!'],
     farewells: ['Adiós', 'Hasta luego', 'Nos vemos', '¡Chao!'],
     help: ['¿En qué puedo ayudarte?', 'Dime cómo puedo ayudarte.', 'Estoy aquí para ayudarte.'],
-    mission: ['mision','La misión de la Municipalidad de San Jerónimo es: "La Municipalidad de San Jerónimo, es una entidad moderna y competitiva, de reconocida imagen y referente Regional, que impulsa la descentralización a través de la Red de Municipalidades del Valle Sur; incorporando su planeación y gestión local, la participación activa y fiscalización de los ciudadanos y ciudadanas; se sustenta en un modo de gestión con acciones permanentes de coordinación, control, monitoreo y evaluación del personal, con la implementación de programas innovadores de simplificación de procedimientos, que permita agilizar y mejorar los procesos burocráticos; con autoridades y funcionarios capacitados y honestos, liderando el desarrollo local con profesionalismo, transparencia, vocación de servicio e identidad institucional."'],
-    vision: ['vision','La visión de la Municipalidad de San Jerónimo es: "Somos una institución al servicio de la comunidad, responsable de generar políticas adecuadas para el emprendimiento de las capacidades empresariales, sociales y políticas; promotora del desarrollo y bienestar del territorio, mediante la administración eficiente y transparente de los recursos públicos y aprovechamiento las oportunidades  de  las  actividades  estrategias  como  el  comercio,  turismo   recreativo  –  gastronómico  y  la agricultura ecológica; haciendo de San Jerónimo un Municipio Líder y competitivo, en el que se puede vivir con dignidad."'],
+    mission: ['La misión de la Municipalidad de San Jerónimo es: "La Municipalidad de San Jerónimo, es una entidad moderna y competitiva, de reconocida imagen y referente Regional, que impulsa la descentralización a través de la Red de Municipalidades del Valle Sur; incorporando su planeación y gestión local, la participación activa y fiscalización de los ciudadanos y ciudadanas; se sustenta en un modo de gestión con acciones permanentes de coordinación, control, monitoreo y evaluación del personal, con la implementación de programas innovadores de simplificación de procedimientos, que permita agilizar y mejorar los procesos burocráticos; con autoridades y funcionarios capacitados y honestos, liderando el desarrollo local con profesionalismo, transparencia, vocación de servicio e identidad institucional."'],
+    vision: ['La visión de la Municipalidad de San Jerónimo es: "Somos una institución al servicio de la comunidad, responsable de generar políticas adecuadas para el emprendimiento de las capacidades empresariales, sociales y políticas; promotora del desarrollo y bienestar del territorio, mediante la administración eficiente y transparente de los recursos públicos y aprovechamiento las oportunidades  de  las  actividades  estrategias  como  el  comercio,  turismo   recreativo  –  gastronómico  y  la agricultura ecológica; haciendo de San Jerónimo un Municipio Líder y competitivo, en el que se puede vivir con dignidad."'],
     default: ['Lo siento, no entendí eso.', 'Podrías repetirlo, por favor?', 'No estoy seguro de entenderte.'],
-    // Añade más categorías y respuestas según sea necesario
 };
 
 // Función para abrir el modal y animar el león
@@ -44,20 +43,25 @@ function sendMessage() {
     const message = document.getElementById('message').value.trim();
     if (!message) return;
 
+    const chatBox = document.getElementById('chat-box');
+
+    // Crear el mensaje del usuario
     const userMessage = document.createElement('div');
     userMessage.classList.add('chat-message', 'user-message');
     userMessage.textContent = message;
-    document.getElementById('chat-box').appendChild(userMessage);
+    chatBox.appendChild(userMessage);
 
+    // Crear el mensaje del bot
     const botMessage = document.createElement('div');
     botMessage.classList.add('chat-message', 'bot-message');
     const response = getBotResponse(message);
     botMessage.textContent = response;
-    document.getElementById('chat-box').appendChild(botMessage);
+    chatBox.appendChild(botMessage);
 
-    // Agregar clase de animación al mensaje del bot
-    botMessage.classList.add('jump');
+    // Desplazar el contenedor del chat hacia abajo para mostrar el nuevo mensaje
+    chatBox.scrollTop = chatBox.scrollHeight;
 
+    // Limpiar el campo de entrada de texto
     document.getElementById('message').value = '';
 }
 
@@ -71,9 +75,9 @@ function getBotResponse(message) {
         return responses.farewells[Math.floor(Math.random() * responses.farewells.length)];
     } else if (message.includes('ayuda') || message.includes('help')) {
         return responses.help[Math.floor(Math.random() * responses.help.length)];
-    } else if (message.includes('mision') || message.includes('mission')) {
+    } else if (message.includes('misión') || (message.includes('mision'))) {
         return responses.mission[Math.floor(Math.random() * responses.mission.length)];
-    } else if (message.includes('vision') || message.includes('vision')) {
+    } else if (message.includes('visión') || (message.includes('vision'))){
         return responses.vision[Math.floor(Math.random() * responses.vision.length)];
     } else {
         return responses.default[Math.floor(Math.random() * responses.default.length)];
